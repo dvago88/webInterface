c11ad0a3b94f4e3c6c31e4d85e7a25d7c34d1610
daniel <dvago1988@gmail.com>
2018-02-19 05:20:06 -0500

    -Autenticación con el servidor rest lograda (aun no hay csrf)

--- out/production/resources/static/javascript/ajax/ajaxHome.js
+++ out/production/resources/static/javascript/ajax/ajaxHome.js
@@ -1,14 +1,11 @@
+let baseUrl = "http://localhost:8090/";
 //Llama a la API para pedir las estaciones segun el centro elegido
 $("select").change(function () {
     let url;
     if ($(this).val() === '1') {
-        url = "http://localhost:8090/stations";
+        url = baseUrl + "stations";
         // url = "https://aqueous-temple-46001.herokuapp.com/stations";
     }
-    //Esto es para csrf cuando se implemente la seguridad
-    /*   let token = $(elem).parent().children("input").attr("value");
-       let header = $(elem).parent().children("input").attr("name");*/
-
     $.getJSON(url, function (data) {
         let numberStations = data.length;
         let contador = 0;
@@ -20,11 +17,6 @@ $("select").change(function () {
         $(".infoCiclas").show();
         $("#total").text(numberStations);
         $("#libres").text(contador);
-
-        /* $("body").append("<div class='container'>" +
-             "<h1 class='stations'>" + numberStations + "</h1>" +
-             "<h1>" + contador + "</h1>" +
-             "</div>")*/
     }).fail(function (jqXHR) {
         console.log(jqXHR);
         $(".infoCiclas").hide();
@@ -32,3 +24,34 @@ $("select").change(function () {
 
 });
 
+$('#loginForm').submit(function (event) {
+    let url;
+    url = baseUrl + "perform_login";
+    event.preventDefault();
+    let username = $('#username').val();
+    let data = 'username=' + username + '&password=' + $('#password').val();
+
+    $.ajax(url, {
+        data: data,
+        timeout: 1000,
+        type: 'POST',
+        crossDomain: true,
+        success: function (data, textStatus, jqXRH) {
+            $(".login-button").append(`<div class="username">${username}</div>`);
+            console.log("clear success");
+        }
+
+    }).done(function (data, textStatus, jqXHR) {
+        console.log("success");
+        console.log(data);
+        console.log(textStatus);
+        console.log(jqXHR);
+        // window.location = preLoginInfo.url;
+
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+        console.log("failure");
+        console.log(textStatus);
+        console.log(jqXHR);
+        console.log(errorThrown);
+    });
+});
--- out/production/resources/static/javascript/ajax/ajaxUser.js
+++ out/production/resources/static/javascript/ajax/ajaxUser.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    let url = "http://localhost:8090/historial/1";
+// let url="https://aqueous-temple-46001.herokuapp.com/historial/1";
+
+    $.getJSON(url, function (data) {
 //  TODO: Hacer el llamado al server para cargar los datos del usuario
+
+    }).fail(function (jqXHR) {
+        console.log(jqXHR);
+    });
+
+
 });
